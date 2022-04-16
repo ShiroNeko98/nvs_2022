242e00364845b3f25f5bb9ad2d43e7dcfcb6e6ac
Quoc Duy Do <duy.doquoc12@gmail.com>
2022-04-16 14:24:05 +0200

    transmitter optional params, readme docu

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
+### Receiver
 1. Start Receiver
    1. javac Receiver.java
    2. java Receiver
 
+### Transmitter
 2. Start Transmitter
    1. javac Transmitter.java
    2. java Transmitter test.txt localhost
+
+java Transmitter <path of file> <ip address> [optional]
+
+Optional:
+-h: print help page
+-s <integer>: size of data, default: 1472
+-p <integer>: port number, default: 1234
--- src/Transmitter.java
+++ src/Transmitter.java
@@ -9,11 +9,12 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 public class Transmitter {
+    private static int DATA_SIZE = 1472;
+    private static int PORT = 1234;
 
     private final DatagramSocket datagramSocket;
     private final InetAddress inetAddress;
 
-    private final int PORT = 1234;
     private final String NULL_TERMINATED = "\u0000";
 
     public Transmitter(DatagramSocket datagramSocket, InetAddress inetAddress) {
@@ -22,10 +23,17 @@ public class Transmitter {
     }
 
     public static void main(String[] args) throws IOException, InterruptedException, NoSuchAlgorithmException {
-        if (args.length != 2) {
+        if (args.length < 2) {
+            if (args[0].equals("-h")) {
+                printHelpText();
+                System.exit(0);
+            }
+
             System.out.println("Not enough or too many arguments");
             System.exit(1);
         }
+
+        setOptionalParameters(args);
 
         // start transmitter
         System.out.println("Starting transmitter ...");
@@ -34,6 +42,31 @@ public class Transmitter {
         transmitter.sendData(args[0]);
 
         System.out.println("File transmitted. Closing program ...");
+    }
+
+    private static void printHelpText() {
+        System.out.print("The program must be executed like: java Transmitter <path of file> <port> [optional]\n\n" +
+                         "Optional:\n" +
+                         "-h ... print help page\n" +
+                         "-s <integer> ... set size of data\n" +
+                         "-p <integer> ... set port");
+    }
+
+    private static void setOptionalParameters(String[] args) {
+        for (int i = 2; i < args.length; i++) {
+            String param = args[i];
+
+            if (param.equals("-s")) {
+                i++;
+                DATA_SIZE = Integer.parseInt(args[i]);
+            } else if (param.equals("-p")) {
+                i++;
+                PORT = Integer.parseInt(args[i]);
+            } else {
+                System.out.println("ERROR: not supported parameter " + param + " found");
+                System.exit(1);
+            }
+        }
     }
 
     private void sendData(String filePath) throws IOException, InterruptedException, NoSuchAlgorithmException {
@@ -47,30 +80,7 @@ public class Transmitter {
             sendAndWait(initialData);
         }
 
-        // send text of file
-        int byteRead = 0;
-        int copyStartIndex = 0;
-        byte[] bytesOfFile = new byte[(int) file.length()];
-
-        FileInputStream fileInputStream = new FileInputStream(file);
-        while (byteRead != file.length()) {
-            byte[] bFile = new byte[256];
-            byteRead += fileInputStream.read(bFile);
-
-            // collect read bytes for md5 hash at the end of transmission
-            for (int i = 0; i < bFile.length; i++) {
-                if (bFile[i] == 0) {
-                    // EOF reached => no need for further copy
-                    break;
-                }
-
-                bytesOfFile[copyStartIndex + i] = bFile[i];
-            }
-            copyStartIndex = byteRead;
-
-            sendAndWait(byteRead + NULL_TERMINATED + Arrays.toString(bFile));
-        }
-        fileInputStream.close();
+        byte[] bytesOfFile = sendFileContent(file);
 
         // send end packet with md5 hash
         MessageDigest md = MessageDigest.getInstance("MD5");
@@ -89,5 +99,42 @@ public class Transmitter {
         datagramSocket.send(datagramPacket);
 
         Thread.sleep(1000);
+    }
+
+    /**
+     * Send content of file after splitting into multiple packets with fixed size.
+     *
+     * @param file file to be sent
+     * @return content of file in bytes
+     * @throws IOException          problem with finding or reading file
+     * @throws InterruptedException problem with sending packets
+     */
+    private byte[] sendFileContent(File file) throws IOException, InterruptedException {
+        int byteRead = 0;
+        int copyStartIndex = 0;
+        byte[] bytesOfFile = new byte[(int) file.length()];
+
+        FileInputStream fileInputStream = new FileInputStream(file);
+        while (byteRead != file.length()) {
+            byte[] bFile = new byte[DATA_SIZE];
+            byteRead += fileInputStream.read(bFile);
+
+            // collect read bytes for md5 hash at the end of transmission
+            for (int i = 0; i < bFile.length; i++) {
+                if (bFile[i] == 0) {
+                    // EOF reached => no need for further copy
+                    break;
+                }
+
+                bytesOfFile[copyStartIndex + i] = bFile[i];
+            }
+            copyStartIndex = byteRead;
+
+            sendAndWait(byteRead + NULL_TERMINATED + Arrays.toString(bFile));
+        }
+
+        fileInputStream.close();
+
+        return bytesOfFile;
     }
 }
