b74f2049895313e96f004ee54c4f1a8ab4cb660f
imnitro99 <80461863+imnitro99@users.noreply.github.com>
2022-04-25 22:20:08 +0200

    Update sender.py

--- src/Python/sender.py
+++ src/Python/sender.py
@@ -1,4 +1,4 @@
- # ----- sender.py ------
+# ----- sender.py ------
 
 # usage python sender.py localhost test.txt
 import os
@@ -6,54 +6,55 @@ from socket import *
 import sys
 import hashlib
 
-s = socket(AF_INET,SOCK_DGRAM)
-host =sys.argv[1]
+s = socket(AF_INET, SOCK_DGRAM)
+host = sys.argv[1]
 port = 11000
-buf =1024
+buf = 1024
 sequenznummer = 0
 
-addr = (host,port)
+addr = (host, port)
 
 path = sys.argv[2]
 size = os.path.getsize(path)
-gesamtanzahl = int(size/buf)
+gesamtanzahl = int(size / buf)
 
-file_name= sys.argv[2].encode('utf-8')
+file_name = sys.argv[2].encode('utf-8')
 
-#----Startpaket senden-----
+# ----Startpaket senden-----
 
-#s.sendto(file_name,addr)
+# s.sendto(file_name,addr)
 startpaket = str(sequenznummer).encode('utf-8') + b'\u0000' + str(gesamtanzahl).encode('utf-8') + b'\u0000' + file_name
+
+
 print("Sending startpacket....")
-s.sendto(startpaket,addr)
+s.sendto(startpaket, addr)
 sequenznummer = sequenznummer + 1
 
-
-#----Open file for md5 hash-----
-f=open(sys.argv[2],"rb")
+# ----Open file for md5 hash-----
+f = open(sys.argv[2], "rb")
 md5_data = f.read(size)
 result = hashlib.md5(md5_data).hexdigest()
 f.close()
 
-#----Open file for transmission-----
+# ----Open file for transmission-----
 ##print(result)
-f=open(sys.argv[2],"rb")
+f = open(sys.argv[2], "rb")
 
 data = f.read(buf)
 packet = str(sequenznummer).encode('utf-8') + b'\u0000' + data
 
 while (data):
     print("Sending....")
-    if(s.sendto(packet,addr)):
+    if (s.sendto(packet, addr)):
         data = f.read(buf)
-        if(data):
-            packet = str(sequenznummer).encode('utf-8') + b'\u0000' + data
+        if (data):
             sequenznummer = sequenznummer + 1
+            packet = str(sequenznummer).encode('utf-8') + b'\u0000' + data
 
-endpacket = result.encode('utf-8') + b'\u0000' + str(gesamtanzahl).encode('utf-8')
+
+endpacket = b'-1' + b'\u0000' + result.encode('utf-8')
 print("Sending endpacket....")
-s.sendto(endpacket,addr)
-
+s.sendto(endpacket, addr)
 
 s.close()
 f.close()
