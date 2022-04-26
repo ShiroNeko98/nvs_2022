97bcda2074c999751d5cf4d68f16f6cc82da183d
imnitro99 <80461863+imnitro99@users.noreply.github.com>
2022-04-26 15:38:28 +0200

    Update receiver.py

--- src/Python/receiver.py
+++ src/Python/receiver.py
@@ -2,7 +2,7 @@
 
 #usage  python receiver.py file/path
 
-
+import hashlib
 from socket import *
 import sys
 import select
@@ -11,25 +11,42 @@ host="0.0.0.0"
 port = 11000
 s = socket(AF_INET,SOCK_DGRAM)
 s.bind((host,port))
+nummer = 0
 
 addr = (host,port)
 buf=2000
-
+file_name,addr = s.recvfrom(buf)
 startpacket,addr = s.recvfrom(buf)
-print("startpacket:" + str(startpacket))
-data,addr = s.recvfrom(buf)
-print("Received File:",data.strip(b'1'))
+#print("startpacket:" + str(startpacket))
+
+print("Received File:",file_name)
 f = open(sys.argv[1],'wb')
 
-
+#-----------First package------------------
+data,addr = s.recvfrom(buf)
+f.write(data[data.find(b"\u0000")+1:].removeprefix(b'u0000'))
 
 try:
-    while(data.decode('utf-8')):
-        f.write(data)
+    while(data):
         s.settimeout(2)
-        data,addr = s.recvfrom(buf)
+        data, addr = s.recvfrom(buf)
+        nummer+=1
+        if(data[0] != 45):  #just checks if first bytearray is -1, aka endpacket
+            f.write(data[data.find(b"\u0000")+1:].removeprefix(b'u0000'))
 except timeout:
     f.close()
     s.close()
-    print("endpacket:" + str(data))
+    #print("endpacket:" + str(data))
     print("File Downloaded")
+    print("Number of packages received: " + str(nummer))
+
+f = open(sys.argv[1], "rb")
+md5_data = f.read()
+result = hashlib.md5(md5_data).hexdigest()
+f.close()
+
+
+data = data[data.find(b"\u0000")+1:].removeprefix(b'u0000')
+
+if data == bytes(result, 'ascii'):
+    print("packet richtig bekommen!!!")
