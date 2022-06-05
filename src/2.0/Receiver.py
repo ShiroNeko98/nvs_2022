494472e6af4de2975a40c82e144fb8455d16322e
imnitro99 <80461863+imnitro99@users.noreply.github.com>
2022-06-05 18:01:26 +0200

    Update Receiver.py

--- src/2.0/Receiver.py
+++ src/2.0/Receiver.py
@@ -3,16 +3,17 @@
 import datetime
 import time
 from socket import *
+import base64
 
 #socket settings
-host='0.0.0.0'
+host='127.0.0.1'
 #port = int(sys.argv[1])
 port = 11000
 s = socket(AF_INET,SOCK_DGRAM)
 s.bind((host,port))
 addr = (host,port)
 port_send = 12000
-buf=5000
+buf=80000
 nummer = 0
 
 t = socket(AF_INET, SOCK_DGRAM)
@@ -20,7 +21,8 @@ a = bytearray(b'')
 #receiving startpacket
 data,addr = s.recvfrom(buf)
 addr2 = (addr[0], port_send)
-t.sendto(b'ACK',addr2)
+
+t.sendto(data.split(b'\x00')[0],addr2)
 temp_data = data
 
 #split startpacket to get the filename and the number of total packets
@@ -36,14 +38,16 @@ f = open(temp_data[1],'wb')
 #-----------First package------------------
 tic = time.perf_counter()
 data,addr = s.recvfrom(buf)
+temp_data = data.split(b'\x00')
 nummer+=1
-t.sendto(b'ACK', addr2)
+t.sendto(temp_data[0], addr2)
 try:
-    while (data):
-        a.extend(data)
+    while base64.b64decode(temp_data[1]):
+        a.extend(base64.b64decode(temp_data[1]))
         s.settimeout(1)
         data, addr = s.recvfrom(buf)
-        t.sendto(b'ACK', addr2)
+        temp_data = data.split(b'\x00')
+        t.sendto(temp_data[0], addr2)
         nummer+=1
 except timeout:
     toc = time.perf_counter()
@@ -59,6 +63,7 @@ time = toc - tic - 1
 temp_size = file_size/1000000
 speed = temp_size/time
 l = open("log.txt",'a')
-l.write("Transmission ip: " + host + "\nTransmission port: " + str(port) + "\nPackets received " + str(nummer) + "/" + str(complete)
-        + "\nLocaltime: " + str(datetime.datetime.now()) + "\nTime needed for transmission: " + str(time) + "\nSpeed : " + str(speed) + "mbps" + "\n--------------------------------------------\n")
+#l.write("Transmission ip: " + host + "\nTransmission port: " + str(port) + "\nPackets received " + str(nummer) + "/" + str(complete)
+        #+ "\nLocaltime: " + str(datetime.datetime.now()) + "\nTime needed for transmission: " + str(time) + "\nSpeed : " + str(speed) + "mbps" + "\n--------------------------------------------\n")
+l.write("\n" + str(speed))
 l.close()
